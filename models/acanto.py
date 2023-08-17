ac4b3807833a8d13707a079d00268f4ad2bc5eaf
jeffrepo <jeffer.leo93@gmail.com>
2023-08-17 09:47:43 -0600

    Cambios voucher

--- models/acanto.py
+++ models/acanto.py
@@ -11,7 +11,6 @@ class AcantoVoucher(models.Model):
     factura = fields.Char(string='Factura')
     descripcion = fields.Char(string='Descripcion')
     fecha = fields.Date(string='Fecha')
-    voucher_no = fields.Char(string='Voucher No')
     cantidad_pago = fields.Float(string='Cantidad')
     banco_id = fields.Many2one('account.journal',string="Banco")
     cuenta_cheque = fields.Float(string='Cuenta No.')
@@ -21,9 +20,18 @@ class AcantoVoucher(models.Model):
     elaborado = fields.Char(string='Elaborado por')
     autorizado = fields.Char(string='Autorizado por')
     recibido = fields.Char(string='Recibido por')
+    linea_ids = fields.One2many('acanto.voucher.linea', 'voucher_id', string="Lineas")
 
     @api.model
     def create(self, vals):
         if vals.get('name', _('New')) == _('New'):
             vals['name'] = self.env['ir.sequence'].next_by_code('acanto.voucher')
         return super().create(vals)
+
+class AcantoVoucherLinea(models.Model):
+    _name = 'acanto.voucher.linea'
+    _description = 'Voucher Linea'
+
+    voucher_id = fields.Many2one('acanto.voucher', string="Voucher")
+    name = fields.Char('Descripcion')
+    total = fields.Float('Total')
